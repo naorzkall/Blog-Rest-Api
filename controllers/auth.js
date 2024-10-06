5d1bfdffb9d22364652b4b2045542c480400e01f
naorz kall <kallnaorz@gmail.com>
2024-10-06 10:36:56 +0300

    kick-off

---
+++ controllers/auth.js
@@ -0,0 +1,99 @@
+const { validationResult, Result } = require('express-validator')
+const bcrypt =  require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+
+const User = require('../models/User');
+
+
+exports.signup = (req,res,next) => {
+    const errors = validationResult(req);
+    if(! errors.isEmpty){
+        const error =  new Error('Validation failed')
+        error.statusCode = 422;
+        // throw the errors array
+        error.data =  errors.array();
+        throw error;
+    }
+
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    bcrypt.hash(password, 12)
+        .then(hashed => {
+            const user =new User({
+                email: email,
+                name: name,
+                password: hashed
+            })
+            return user.save();
+        })
+        .then(result => {
+            res.status(201).json({
+                message: 'User Created.',
+                userId : result._id
+            })
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+}
+
+exports.login = (req,res,next)=>{
+    const email =  req.body.email;
+    const password =  req.body.password;
+    let loadeduser;
+
+    User.findOne({email: email })
+        .then(user => {
+            if(!user){
+                const error =  new Error('A user can not be found');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadeduser = user;
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual => {
+            if (!isEqual){
+                const error = new Error('wrong password');
+                error.statusCode = 404;
+                throw error;
+            }
+            /*   generate a new token
+               - jwt.sign() method creates a new signature and packs that into a new json web token
+               - we can add any we want into the token like (email,userId ....etc)
+               - should not store the raw password, beacuse that would be returned to the frontend (not ideal)
+               - second argument is the secret => the private key which used signing
+                    and that is now only known to the server, and therefore you can't fake that token on the client side
+               - for secret ypu want to use a longer string
+               - third argument is the expired time (the token become invalid after that time)
+                    now this is a mechanism you should add beacuse the token is stored in the client
+                    technically, that token could be stolen, so after the time expires will not be able to use it
+               
+            */
+            const token = jwt.sign({
+                email: loadeduser.email,
+                userId: loadeduser._id.toString()
+            },
+            'somesupersecretsecret',
+            { expiresIn: '1h'}
+            );
+            res.status(200).json({
+                token : token,
+                // in react app I will looking for that id
+                userId : loadeduser._id.toString()
+            })
+
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500;
+            }
+            next();
+        })
+}
---
+++ controllers/feed.js
@@ -0,0 +1,203 @@
+const fs = require('fs');
+const path = require('path');
+
+const { validationResult } = require('express-validator');
+
+const Post = require('../models/post');
+
+exports.getPosts = (req, res, next) => {
+    /*  
+        -json() is an express function allow us to return a response with json data with right header and so on
+        -we can pass a normal js object to json() and it will be converted to json and sent back to the client who send the request
+        -sending a json response, and the clinet will take it as a json and render it based on the error code also 
+    */
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
+    Post.find()
+        .countDocuments()
+        .then(count => {
+        totalItems = count;
+        return Post.find()
+            .skip((currentPage - 1) * perPage)
+            .limit(perPage);
+        })
+        .then(posts => {
+        res
+            .status(200)
+            .json({
+            message: 'Fetched posts successfully.',
+            posts: posts,
+            totalItems: totalItems
+            });
+        })
+        .catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+        });
+};
+
+exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, enterd data is incorrect.');
+        // adding my own custom property (you can named whatever you want)
+        error.statusCode = 422;
+        /* -throw an error
+                it will automatically exit the function execution here 
+                and instead will try to reach the next error handling mildware
+                provided by our express application
+         */
+        throw error;
+    }
+    if(!req.file){
+        const error = new Error('No image provided.');
+        error.statusCode = 404;
+        throw error
+    }
+    /*
+        once there no error that mean that multure was able to extract a valid file,
+        so you can access file.path which multer generates and which holds the path
+        to the path to the file as it was stored on the server
+    */
+    const imageUrl = req.file.path.replace("\\" ,"/");
+    const title = req.body.title;
+    const content = req.body.content; 
+
+    //create post
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: imageUrl,
+        creator: { name: 'Naorz Kall' }
+      });
+    // save to mongodb
+    post.save()
+        .then(result => {
+        // 200 is just success, 201 is a code to tell the clinet that a resourse was created
+        res.status(201).json({
+            message: 'Post created successfully!',
+            post: result
+        });
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500;
+            }
+            /* 
+                and we learned that since we inside a promise chain(asynch code snippet),
+                throwing an error will not do the trick
+                so we have to use next()
+            */
+            next();
+
+        });
+};
+
+exports.getPost = (req,res,next)=>{
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if(!post){
+                const error  = new Error('Could not find post.');
+                error.statusCode = 404;
+                /* - why we through an error if we are on Asynch code snnipt instead next()?
+                        when we throw an error here the error will be passed as an error to the catch block 
+                 */
+                throw error;
+            }
+            res.status(200).json({message: 'Post fetched.',post : post})
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500;
+            }
+            next();
+            
+        });
+
+}
+
+exports.updatePost = (req,res,next)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, enterd data is incorrect.');
+        error.statusCode = 422;
+        throw error;
+    }
+    const postId = req.params.postId;
+    const title = req.body.title;
+    const content = req.body.content;
+    // will hold the old url
+    let imageUrl = req.body.image;
+    // will hold the uploaded file if the user upload
+    if(req.file){
+        imageUrl = req.file.path.replace("\\" ,"/");
+    }
+    if(!imageUrl){
+        const error =  new Error('No file picked.');
+        error.statusCode = 422;
+        throw error;
+    }
+    Post.findById(postId)
+        .then(post => {
+            if(!post){
+                const error  = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            if(imageUrl !== post.imageUrl){
+                clearImage(post.imageUrl);
+            }
+            post.title = title;
+            post.imageUrl = imageUrl;
+            post.content = content;
+            return post.save();
+        })
+        .then(result => {
+            res.status(200).json({message:'Post updated!', post: result })
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500;
+            }
+            next();
+        });
+
+}
+
+exports.deletePost = (req,res,next)=>{
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if(!post){
+                const error  = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            // check login user
+            clearImage(post.imageUrl);
+            return Post.findByIdAndDelete(postId);
+        })
+        .then(result => {
+            // console.log(result);
+            res.status(200).json({
+                message: 'Delete post done.'
+            })
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500;
+            }
+            next();
+        });
+
+}
+
+const clearImage = filePath => {
+    // '..' to up one foleder because we are not on the root folder, we are the controller folder
+    filePath = path.join(__dirname,'..' ,filePath);
+    fs.unlink(filePath, err => {console.log(err);});
+};
